69b99bbc01e24e03cdd48f3a75d91d36b6b8bd62
Fraser <tomatoesneverlie@gmail.com>
2022-05-24 21:28:51 -0400

    progress

--- render/capture.py
+++ render/capture.py
@@ -3,10 +3,13 @@
 # pip install selenium
 # brew install chromedriver (or whatever's up on your system)
 
+SCALE = 0.3# 4.0
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import selenium.webdriver.common as c
 import time
+import os
 
 chrome_options = Options()
 #  chrome_options.headless = True
@@ -14,14 +17,16 @@ chrome_options = Options()
 
 driver = webdriver.Chrome('chromedriver', options=chrome_options)
 
-driver.get('http://[::]:8000/kb.html#/')
+# print script directory
+driver.get('file://' + os.path.dirname(os.path.realpath(__file__)) + '/kle/kb.html')
 
 time.sleep(3)
 
 #  root = driver.find_element(by=c.by.By.TAG_NAME, value='body')
 
 # zoom in
-#  driver.execute_script("document.body.style.zoom='400%'")
+#  driver.execute_script("document.body.style.zoom='40%'")
+driver.execute_script(f'document.body.style.zoom = {SCALE};')
 
 with open('layers.json', 'r') as f:
     driver.execute_script(f'angular.element(document.getElementsByTagName("body")).scope().deserializeAndRender({f.read()}, true)')
